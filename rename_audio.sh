28487dabe7bc947e6ec2d54aa96ad75e920466fd
Bastian <51163929+Bastians-Bits@users.noreply.github.com>
2025-07-26 03:03:03 +0200

    Add files via upload

---
+++ rename_audio.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file_extension=$(echo ${1##*.})
+
+# List all audio streams
+ffprobe -v error -select_streams a -show_entries stream=index:stream_tags=language:stream=channel_layout -of csv=p=0 "$1"
+
+printf "Input the index of the stream to rename:\n"
+read stream_index
+printf "Input the new langauge code:\n"
+read language_code
+
+ffmpeg -i "$1" -metadata:s:${stream_index} language=${language_code} -codec copy -map 0 "${1%.${file_extension}}.tmp.${file_extension}"
+
+mv "${1%.${file_extension}}.tmp.${file_extension}" "$1"
